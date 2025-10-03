f8564c9d3b645d2f15258897eaca8367e384a1ea
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2025-10-03 13:43:54 +0200

    feat: Work on the database system

---
+++ src/database/connection.rs
@@ -0,0 +1,44 @@
+use std::env;
+
+use diesel::{Connection, RunQueryDsl, SqliteConnection, sql_query};
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
+use eyre::Result;
+
+use crate::DATABASE_URL_ENVIROMENT;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
+
+pub struct Database {
+    connection: SqliteConnection,
+}
+
+impl Database {
+    pub fn establish() -> Result<Self> {
+        let url = env::var(DATABASE_URL_ENVIROMENT)?;
+
+        // Run migrations
+        Self::run_migrations(&url)?;
+
+        // Establish a connection to the sqlite database
+        let connection = SqliteConnection::establish(&url)?;
+        Ok(Self { connection })
+    }
+
+    fn run_migrations(url: &str) -> Result<()> {
+        // Establish a connection to the sqlite database (this will create a new one, if it does
+        // not exist, and exit if there is an error).
+        let mut connection = SqliteConnection::establish(url)?;
+
+        // Run the migrations after successfully establishing a connection
+        // Disable Foreign Key Checks during migration
+        // Scoped to a connection.
+        sql_query("PRAGMA foreign_keys = OFF")
+            .execute(&mut connection)
+            .expect("Failed to disable Foreign Key Checks during migrations");
+
+        connection
+            .run_pending_migrations(MIGRATIONS)
+            .expect("Failed to run migrations");
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use eyre::Result;
 
-use crate::route::launch_rocket;
+use crate::{database::connection::Database, route::launch_rocket};
 
 mod component;
 mod database;
 mod route;
 
+pub const DATABASE_URL_ENVIROMENT: &str = "DATABASE_URL";
+
 #[rocket::main]
 async fn main() -> Result<()> {
+    color_eyre::install()?;
+    dotenvy::dotenv()?;
+
+    let database = Database::establish()?;
+
     launch_rocket().await?;
     Ok(())
 }
